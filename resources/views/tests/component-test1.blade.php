439833d3dc6e15dad96793015894b0197594a27a
naonao-aaa <naonao1814naonao@gmail.com>
2021-09-23 10:18:34 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,5 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー１</x-slot>
 コンポーネントテスト１
-  <x-tests.card title="タイトル１" content="本文１" />
+  <x-tests.card title="タイトル１" content="本文１" :message="$message" />
 </x-tests.app>
