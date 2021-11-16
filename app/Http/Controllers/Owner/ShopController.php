8bccf3426b042a560d3ebfea22eff8dd63a46d26
naonao-aaa <naonao1814naonao@gmail.com>
2021-11-16 23:42:42 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -47,12 +49,19 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image; //一時保存
 
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            Storage::putFile('public/shops', $imageFile);
+            //Storage::putFile('public/shops', $imageFile); リサイズなしの場合
+            $ﬁleName = uniqid(rand() . '_');
+            $extension = $imageFile->extension();
+            $ﬁleNameToStore = $ﬁleName . '.' . $extension;
+
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+
+            Storage::put('public/shops/' . $ﬁleNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
