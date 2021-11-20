be9798531a41c75217ed6bfa744e1ded85db64c7
naonao-aaa <naonao1814naonao@gmail.com>
2021-11-20 10:51:11 +0900

    サービスへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -54,14 +55,8 @@ class ShopController extends Controller
         $imageFile = $request->image; //一時保存
 
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            //Storage::putFile('public/shops', $imageFile); リサイズなしの場合
-            $ﬁleName = uniqid(rand() . '_');
-            $extension = $imageFile->extension();
-            $ﬁleNameToStore = $ﬁleName . '.' . $extension;
 
-            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-
-            Storage::put('public/shops/' . $ﬁleNameToStore, $resizedImage);
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
         }
 
         return redirect()->route('owner.shops.index');
