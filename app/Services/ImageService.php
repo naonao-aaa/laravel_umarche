e26661508c632cec605f1f90d5c67d9b54305793
naonao-aaa <naonao1814naonao@gmail.com>
2021-11-23 16:52:39 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -10,12 +10,19 @@ class ImageService
 {
     public static function upload($imageFile, $folderName)
     {
+        //dd($imageFile['image']);
+
+        if (is_array($imageFile)) {
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
 
         $ﬁleName = uniqid(rand() . '_');
-        $extension = $imageFile->extension();
+        $extension = $file->extension();
         $ﬁleNameToStore = $ﬁleName . '.' . $extension;
 
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+        $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
 
         Storage::put('public/' . $folderName . '/' . $ﬁleNameToStore, $resizedImage);
 
