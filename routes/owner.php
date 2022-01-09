355aa61507b65bf540c1c669236de59230c7fb7e
naonao-aaa <naonao1814naonao@gmail.com>
2022-01-09 17:16:46 +0900

    route修正

--- routes/owner.php
+++ routes/owner.php
@@ -26,9 +26,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('owner.welcome');
-});
+//Route::get('/', function () {
+//  return view('owner.welcome');
+//});
 
 Route::prefix('shops')->middleware('auth:owners')->group(function () {
   Route::get('index', [ShopController::class, 'index'])->name('shops.index');
@@ -50,12 +50,12 @@ Route::get('/dashboard', function () {
 
 
 
-Route::get('/register', [RegisteredUserController::class, 'create'])
-  ->middleware('guest')
-  ->name('register');
+//Route::get('/register', [RegisteredUserController::class, 'create'])
+//  ->middleware('guest')
+//  ->name('register');
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
-  ->middleware('guest');
+//Route::post('/register', [RegisteredUserController::class, 'store'])
+//  ->middleware('guest');
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
   ->middleware('guest')
